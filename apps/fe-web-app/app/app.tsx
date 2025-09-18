3ce7ab674fc45835f11eb511d9e73950a42f1dbb
AbandonedDemon <hoanghiepgamer@gmail.com>
2025-09-19 00:20:44 +0700

    test: trigger updated CI workflow

--- apps/fe-web-app/app/app.tsx
+++ apps/fe-web-app/app/app.tsx
@@ -4,6 +4,9 @@ export function App() {
   return (
     <div>
       <NxWelcome title="@fe-web-app/fe-web-app" />
+      <div>
+        This message is used to confirm that the CI workflow is working properly!
+      </div>
     </div>
   );
 }
