48041bf5bfdc299daa16cff53a4f69603e5f2592
baole405 <baolttse184569@fpt.edu.vn>
2025-09-18 22:08:10 +0700

    chore(tooling): setup commitlint and husky hooks

---
+++ commitlint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // You can customize scopes or rules here
+  // rules: {
+  //   'scope-enum': [2, 'always', ['app', 'api', 'deps']]
+  // }
+};
