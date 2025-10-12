f62f89da1c582f8851dd3148a9742f021a0b8694
baole <tommydao2000@gmail.com>
2025-10-12 17:15:27 +0700

    test: update dashboard greeting assertion

--- .apps/fe-web-app-e2e/src/e2e/app.cy.ts
+++ .apps/fe-web-app-e2e/src/e2e/app.cy.ts
@@ -3,11 +3,11 @@ import { getGreeting } from '../support/app.po';
 describe('@fe-web-app/fe-web-app-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display the dashboard heading', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+    getGreeting().contains(/Operations dashboard/i);
   });
 });
