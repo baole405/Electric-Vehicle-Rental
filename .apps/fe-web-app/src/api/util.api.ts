efd0b21798ec44bd6b136d3c4745679498096471
ArmyNhuQuynh <quynhntn2309@gmail.com>
2025-10-12 23:39:26 +0700

    feat: get all

---
+++ .apps/fe-web-app/src/api/util.api.ts
@@ -0,0 +1,5 @@
+
+
+export const API_SUFFIX = {
+  VEHICEL_API: "/api/vehicles",
+};
---
+++ .apps/fe-web-app/src/schema/common/response.type.ts
@@ -0,0 +1,18 @@
+export type BaseResponse<T> = {
+  // status: number;
+  // message: string;
+  data: T;
+};
+
+export type PaginationResponse<T> = {
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  last: boolean;
+  sort: string;
+  content: T[];
+};
+
+export type ErrorResponse<T> = BaseResponse<T>;
