344cab0338b7ed5f3d6fc023fb2313e4514de87c
baole405 <baolttse184569@fpt.edu.vn>
2025-10-12 21:15:13 +0700

    ci: remove e2e step

--- .apps/fe-web-app/src/routes/route.utils.ts
+++
@@ -1,7 +0,0 @@
-export const getRelativePath = (fullPath: string, basePath: string): string => {
-  if (fullPath.startsWith(basePath)) {
-    const relative = fullPath.slice(basePath.length);
-    return relative.replace(/^\/+/, "");
-  }
-  return fullPath;
-};
