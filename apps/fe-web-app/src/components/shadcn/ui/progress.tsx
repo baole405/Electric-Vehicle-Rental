9504f47bf73719c097f22ab1d793571f0d42ae8e
baole405 <baolttse184569@fpt.edu.vn>
2025-10-12 19:40:16 +0700

    fix: shadcn card config

--- apps/fe-web-app/src/components/shadcn/ui/progress.tsx
+++ apps/fe-web-app/src/components/shadcn/ui/progress.tsx
@@ -1,13 +1,20 @@
-import { Progress as ProgressPrimitive } from "radix-ui";
-import * as React from "react";
+import * as ProgressPrimitive from '@radix-ui/react-progress';
+import * as React from 'react';
 
-import { cn } from "@/utils/utils";
+import { cn } from '@/utils/utils';
 
-function Progress({ className, value, ...props }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+function Progress({
+  className,
+  value,
+  ...props
+}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
-      className={cn("bg-primary/20 relative h-2 w-full overflow-hidden rounded-full", className)}
+      className={cn(
+        'bg-primary/20 relative h-2 w-full overflow-hidden rounded-full',
+        className
+      )}
       {...props}
     >
       <ProgressPrimitive.Indicator
