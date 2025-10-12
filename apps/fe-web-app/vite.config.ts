ed599f8d2ace72d848e00a4b7fc3c459dc958bd4
baole405 <baolttse184569@fpt.edu.vn>
2025-10-12 19:58:48 +0700

    docs: config ts

--- apps/fe-web-app/vite.config.ts
+++ apps/fe-web-app/vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig(() => ({
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+      '@/components': resolve(__dirname, './src/components'),
+      '@/utils': resolve(__dirname, './src/utils'),
+      '@/assets': resolve(__dirname, './src/assets'),
     },
     extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
     preserveSymlinks: false,
