58dce28aec1371ec6d90bec1ad4af2e6c3481110
baole405 <baolttse184569@fpt.edu.vn>
2025-11-21 23:08:51 +0700

    docs: update readme

--- .apps/fe-web-app/src/schema/common/vehicle-status.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const VehicleStatusSchema = z.enum([
-  "available",
-  "rented",
-  "maintenance",
-]);
-
-export type TVehicleStatus = z.infer<typeof VehicleStatusSchema>;
